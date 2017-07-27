bd81f5827a27248244b730a1145118fe741154a3
surgai <sketbordest@mail.ru>
2017-07-27 07:49:37 +0300

    Creating Contact data model

---
+++ RiseApps/Contact.swift
@@ -0,0 +1,18 @@
+//
+//  Contact.swift
+//  RiseApps
+//
+//  Created by Max Surgai on 27.07.17.
+//  Copyright © 2017 Max Surgai. All rights reserved.
+//
+
+import RealmSwift
+
+class Contact: Object {
+    
+    dynamic var name = ""
+    dynamic var surname = ""
+    dynamic var patronymic = ""
+    dynamic var phone = ""
+    dynamic var markNumber = 0
+}
